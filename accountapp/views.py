1cd16e04d9bd09ff9e298ff25589401c61f3e317
suenyeo <suenyeo@daeeunelec.com>
2020-12-30 22:48:38 +0900

    account CBV(create)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from .models import Helloworld
 
@@ -19,3 +22,11 @@ def hello_world(request):
         hello_world_list = Helloworld.objects.all()
 
         return render(request, 'accountapp/hello_world.html', context={'context': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = "accountapp/create.html"
+
