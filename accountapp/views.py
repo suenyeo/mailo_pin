f869911eb0db31f5216f8f2719d27a16705c1bf3
suenyeo <suenyeo@daeeunelec.com>
2020-12-30 17:50:40 +0900

    model save

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Helloworld
 
 # Create your views here.
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST SUC!!'})
+        temp = request.POST.get("hello_world_input")
+        new_hello_world = Helloworld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'context': new_hello_world})
 
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET SUC!!'})
+        return render(request, 'accountapp/hello_world.html', context={'context': 'GET SUC!!'})
