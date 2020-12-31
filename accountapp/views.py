568650d36128be9361c4525e96784d30966b474b
suenyeo <suenyeo@daeeunelec.com>
2020-12-31 11:16:22 +0900

    add authentication check

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -13,20 +13,21 @@ from .templates.accountapp.forms import AccountUpdateForm
 
 
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get("hello_world_input")
-        new_hello_world = Helloworld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get("hello_world_input")
+            new_hello_world = Helloworld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        hello_world_list = Helloworld.objects.all()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            hello_world_list = Helloworld.objects.all()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
+        else:
+            hello_world_list = Helloworld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'context': hello_world_list})
     else:
-        hello_world_list = Helloworld.objects.all()
-
-        return render(request, 'accountapp/hello_world.html', context={'context': hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
@@ -42,7 +43,9 @@ class AccountDetailView(LoginRequiredMixin, DetailView):
     template_name = 'accountapp/detail.html'
 
 
-class AccountUpdateView(UpdateView):
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
+    login_url =reverse_lazy('accountapp:login')
+
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
@@ -50,8 +53,20 @@ class AccountUpdateView(UpdateView):
     template_name = "accountapp/update.html"
 
 class AccountDeleteView(DeleteView):
+
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = "accountapp/delete.html"
 
+    # LoginRequiredMixin을 사용하지 않는 경우 GET/POST 함수를 상속하여 유저권한 확인 추가하는 방법
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else: 
+            return HttpResponseForbidden()
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
