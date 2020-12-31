fcd6a53bd4ee72a5e0c9a57397b7c702d28a650d
suenyeo <suenyeo@daeeunelec.com>
2020-12-31 11:34:27 +0900

    add decorator and authentication

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,33 +1,33 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from .decorators import account_ownership_requried
 from .models import Helloworld
-
-# Create your views here.
 from .templates.accountapp.forms import AccountUpdateForm
 
+has_ownership = [login_required, account_ownership_requried]
 
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get("hello_world_input")
-            new_hello_world = Helloworld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+    if request.method == "POST":
+        temp = request.POST.get("hello_world_input")
+        new_hello_world = Helloworld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            hello_world_list = Helloworld.objects.all()
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        hello_world_list = Helloworld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        else:
-            hello_world_list = Helloworld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'context': hello_world_list})
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = Helloworld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'context': hello_world_list})
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -35,31 +35,33 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = "accountapp/create.html"
 
-class AccountDetailView(LoginRequiredMixin, DetailView):
-    login_url =reverse_lazy('accountapp:login')
-
+@method_decorator(has_ownership, 'get')
+class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-
-class AccountUpdateView(LoginRequiredMixin, UpdateView):
-    login_url =reverse_lazy('accountapp:login')
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = "accountapp/update.html"
 
-class AccountDeleteView(DeleteView):
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = "accountapp/delete.html"
 
-    # LoginRequiredMixin을 사용하지 않는 경우 GET/POST 함수를 상속하여 유저권한 확인 추가하는 방법
+    """
+    LoginRequiredMixin을 사용하지 않는 경우 GET/POST 함수를 상속하여 유저권한 확인 추가하는 방법
+      > 데코레이터를 이용해서 GET/POST에 권한 체크 방법 추가하는것으료 변경
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated and self.get_object() == self.request.user:
             return super().get(*args, **kwargs)
@@ -70,3 +72,4 @@ class AccountDeleteView(DeleteView):
             return super().get(*args, **kwargs)
         else:
             return HttpResponseForbidden()
+    """
