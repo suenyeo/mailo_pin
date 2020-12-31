014c37b8c0fad7ee69fbabfd41c219d944cfddd2
suenyeo <suenyeo@daeeunelec.com>
2020-12-31 10:04:32 +0900

    add account detail and LoginRequiredMixin

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .models import Helloworld
 
@@ -29,5 +30,12 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = "accountapp/create.html"
+
+class AccountDetailView(LoginRequiredMixin, DetailView):
+    login_url =reverse_lazy('accountapp:login')
+    
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
 
 
