603c3bded33df9e9371eca8c37da73bbe4d4391e
suenyeo <suenyeo@daeeunelec.com>
2021-01-02 17:31:04 +0900

    get_query in CBV

--- subscriptionapp/urls.py
+++ subscriptionapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscriptionapp.views import SubscriptionRedirectView
+from subscriptionapp.views import SubscriptionRedirectView, SubscriptionListView
 
 app_name = 'subscriptionapp'
 
 urlpatterns=[
-    path('subscription/', SubscriptionRedirectView.as_view(),name='subscription')
+    path('subscription/', SubscriptionRedirectView.as_view(),name='subscription'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
